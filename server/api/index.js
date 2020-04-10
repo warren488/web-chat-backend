d3c62514c332566cd91378a94d9b320c3a083718
warren488 <warren.s.488@gmail.com>
2020-04-10 16:50:09 -0400

    add new route for getting last message and a list of all friends

--- server/api/index.js
+++ server/api/index.js
@@ -1,12 +1,15 @@
 let router = require("express").Router()
-let {login, createUser, authenticate, logout, addFriend, chatRedirect, getMessages} = require('../services')
+let {login, createUser, authenticate, logout, addFriend, getFriends, chatRedirect, getMessages, getLastMessage} = require('../services')
 
 router.post('/login', login)
 router.post('/signup', createUser)
 router.delete('/users/me/token', authenticate, logout)
 router.post('/users/me/friends', authenticate, addFriend)
-router.post('/users/me/chat', authenticate, chatRedirect)
+router.get('/users/me/friends', authenticate, getFriends)
 router.get('/users/me/:friendship_id/messages', authenticate, getMessages)
+router.get('/users/me/:friendship_id/lastmessage', authenticate, getLastMessage)
 
+// redirect routes
+router.post('/users/me/chat', authenticate, chatRedirect)
 module.exports = router
 
