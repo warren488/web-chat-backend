48245a48f91185ad6c4b44155529d422bee53d60
warren488 <warren.s.488@gmail.com>
2020-05-18 12:31:06 -0400

    test new way of getting auth

--- server/services/auth.js
+++ server/services/auth.js
@@ -1,8 +1,23 @@
 const admin = require('firebase-admin');
 const credCert = require('../config/firebase');
 
+let credTest = `{
+    "type": "${process.env.firebase_type}",
+    "project_id": "${process.env.firebase_project_id}",
+    "private_key_id": "${process.env.firebase_private_key_id}",
+    "private_key": ${JSON.stringify(process.env.firebase_private_key)},
+    "client_email": "${process.env.firebase_client_email}",
+    "client_id": "${process.env.firebase_client_id}",
+    "auth_uri": "${process.env.firebase_auth_uri}",
+    "token_uri": "${process.env.firebase_token_uri}",
+    "auth_provider_x509_cert_url": "${process.env.firebase_auth_provider_x509_cert_url}",
+    "client_x509_cert_url": "${process.env.firebase_client_x509_cert_url}"
+  }`;
+  
+  let creds = JSON.parse(credTest)
+
 admin.initializeApp({
-  credential: admin.credential.cert(credCert),
+  credential: admin.credential.cert(creds),
 });
 
 module.exports = {
